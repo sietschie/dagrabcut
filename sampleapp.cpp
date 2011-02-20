93dee5dc29e38233886abfe4cf5a7f00823ea168
goering <goering@minet.uni-jena.de>
2011-02-20 20:08:27 +0100

    removed unneeded interactivity

--- sampleapp.cpp
+++ sampleapp.cpp
@@ -9,30 +9,12 @@ using namespace cv;
 
 void help()
 {
-    cout << "\nThis program demonstrates GrabCut segmentation -- select an object in a region\n"
-    		"and then grabcut will attempt to segment it out.\n"
-    		"Call:\n"
+    cout << "Call:\n"
     		"./grabcut <image_name> <mask_name>\n"
         "\nHot keys: \n"
         "\tESC - quit the program\n"
-        "\tr - restore the original image\n"
-        "\tn - next iteration\n"
-        "\n"
-        "\tCTRL+left mouse button - set GC_BGD pixels\n"
-        "\tSHIFT+left mouse button - set CG_FGD pixels\n"
-        "\n"
-        "\tCTRL+right mouse button - set GC_PR_BGD pixels\n"
-        "\tSHIFT+right mouse button - set CG_PR_FGD pixels\n" << endl;
+        "\tn - next iteration\n" << endl;
 }
-
-const Scalar RED = Scalar(0,0,255);
-const Scalar PINK = Scalar(230,130,255);
-const Scalar BLUE = Scalar(255,0,0);
-const Scalar LIGHTBLUE = Scalar(255,255,160);
-const Scalar GREEN = Scalar(0,255,0);
-
-const int BGD_KEY = CV_EVENT_FLAG_CTRLKEY;
-const int FGD_KEY = CV_EVENT_FLAG_SHIFTKEY;
 
 void getBinMask( const Mat& comMask, Mat& binMask )
 {
@@ -46,44 +28,21 @@ void getBinMask( const Mat& comMask, Mat& binMask )
 class GCApplication
 {
 public:
-    enum{ NOT_SET = 0, IN_PROCESS = 1, SET = 2 };
-    static const int radius = 2;
-    static const int thickness = -1;
-
-    void reset();
     void setImageAndWinName( const Mat& _image, const string& _winName, Mat& _mask );
     void showImage() const;
-    void mouseClick( int event, int x, int y, int flags, void* param );
     int nextIter();
     int getIterCount() const { return iterCount; }
 private:
-    void setRectInMask();
-    void setLblsInMask( int flags, Point p, bool isPr );
-
     const string* winName;
     const Mat* image;
     Mat input_mask;
     Mat mask;
     Mat bgdModel, fgdModel;
 
-    uchar lblsState, prLblsState;
     bool isInitialized;
 
-    vector<Point> fgdPxls, bgdPxls, prFgdPxls, prBgdPxls;
     int iterCount;
 };
-
-void GCApplication::reset()
-{
-    mask = input_mask.clone();
-    bgdPxls.clear(); fgdPxls.clear();
-    prBgdPxls.clear();  prFgdPxls.clear();
-
-    isInitialized = false;
-    lblsState = NOT_SET;
-    prLblsState = NOT_SET;
-    iterCount = 0;
-}
 
 void GCApplication::setImageAndWinName( const Mat& _image, const string& _winName, Mat& _mask  )
 {
@@ -93,7 +52,7 @@ void GCApplication::setImageAndWinName( const Mat& _image, const string& _winNam
     winName = &_winName;
     //mask.create( image->size(), CV_8UC1);
     input_mask = _mask; // TODO: is it good to do it that way? or better pointer to mask?
-    reset();
+    mask = input_mask.clone();
 }
 
 void GCApplication::showImage() const
@@ -111,99 +70,7 @@ void GCApplication::showImage() const
         image->copyTo( res, binMask );
     }
 
-    vector<Point>::const_iterator it;
-    for( it = bgdPxls.begin(); it != bgdPxls.end(); ++it )
-        circle( res, *it, radius, BLUE, thickness );
-    for( it = fgdPxls.begin(); it != fgdPxls.end(); ++it )
-        circle( res, *it, radius, RED, thickness );
-    for( it = prBgdPxls.begin(); it != prBgdPxls.end(); ++it )
-        circle( res, *it, radius, LIGHTBLUE, thickness );
-    for( it = prFgdPxls.begin(); it != prFgdPxls.end(); ++it )
-        circle( res, *it, radius, PINK, thickness );
-
     imshow( *winName, res );
-}
-
-void GCApplication::setLblsInMask( int flags, Point p, bool isPr )
-{
-    vector<Point> *bpxls, *fpxls;
-    uchar bvalue, fvalue;
-    if( !isPr )
-    {
-        bpxls = &bgdPxls;
-        fpxls = &fgdPxls;
-        bvalue = GC_BGD;
-        fvalue = GC_FGD;
-    }
-    else
-    {
-        bpxls = &prBgdPxls;
-        fpxls = &prFgdPxls;
-        bvalue = GC_PR_BGD;
-        fvalue = GC_PR_FGD;
-    }
-    if( flags & BGD_KEY )
-    {
-        bpxls->push_back(p);
-        circle( mask, p, radius, bvalue, thickness );
-    }
-    if( flags & FGD_KEY )
-    {
-        fpxls->push_back(p);
-        circle( mask, p, radius, fvalue, thickness );
-    }
-}
-
-void GCApplication::mouseClick( int event, int x, int y, int flags, void* )
-{
-    // TODO add bad args check
-    switch( event )
-    {
-    case CV_EVENT_LBUTTONDOWN: // set rect or GC_BGD(GC_FGD) labels
-        {
-            bool isb = (flags & BGD_KEY) != 0,
-                 isf = (flags & FGD_KEY) != 0;
-            if ( (isb || isf) )
-                lblsState = IN_PROCESS;
-        }
-        break;
-    case CV_EVENT_RBUTTONDOWN: // set GC_PR_BGD(GC_PR_FGD) labels
-        {
-            bool isb = (flags & BGD_KEY) != 0,
-                 isf = (flags & FGD_KEY) != 0;
-            if ( (isb || isf) )
-                prLblsState = IN_PROCESS;
-        }
-        break;
-    case CV_EVENT_LBUTTONUP:
-        if( lblsState == IN_PROCESS )
-        {
-            setLblsInMask(flags, Point(x,y), false);
-            lblsState = SET;
-            showImage();
-        }
-        break;
-    case CV_EVENT_RBUTTONUP:
-        if( prLblsState == IN_PROCESS )
-        {
-            setLblsInMask(flags, Point(x,y), true);
-            prLblsState = SET;
-            showImage();
-        }
-        break;
-    case CV_EVENT_MOUSEMOVE:
-        if( lblsState == IN_PROCESS )
-        {
-            setLblsInMask(flags, Point(x,y), false);
-            showImage();
-        }
-        else if( prLblsState == IN_PROCESS )
-        {
-            setLblsInMask(flags, Point(x,y), true);
-            showImage();
-        }
-        break;
-    }
 }
 
 int GCApplication::nextIter()
@@ -221,18 +88,14 @@ int GCApplication::nextIter()
     }
     iterCount += max_iterations;
 
-    bgdPxls.clear(); fgdPxls.clear();
-    prBgdPxls.clear(); prFgdPxls.clear();
+    FileStorage fs("test.yml", FileStorage::WRITE);
+    fs << "mask" << mask;
+    
 
     return iterCount;
 }
 
 GCApplication gcapp;
-
-void on_mouse( int event, int x, int y, int flags, void* param )
-{
-    gcapp.mouseClick( event, x, y, flags, param );
-}
 
 int main( int argc, char** argv )
 {
@@ -278,38 +141,23 @@ int main( int argc, char** argv )
 
     const string winName = "image";
     cvNamedWindow( winName.c_str(), CV_WINDOW_AUTOSIZE );
-    cvSetMouseCallback( winName.c_str(), on_mouse, 0 );
 
     gcapp.setImageAndWinName( image, winName, mask2 );
     gcapp.showImage();
 
-    for(;;)
+    cvWaitKey(0);
+
+    int iterCount = gcapp.getIterCount();
+    cout << "<" << iterCount << "... ";
+    int newIterCount = gcapp.nextIter();
+    if( newIterCount > iterCount )
     {
-        int c = cvWaitKey(0);
-        switch( (char) c )
-        {
-        case '\x1b':
-            cout << "Exiting ..." << endl;
-            goto exit_main;
-        case 'r':
-            cout << endl;
-            gcapp.reset();
-            gcapp.showImage();
-            break;
-        case 'n':
-            int iterCount = gcapp.getIterCount();
-            cout << "<" << iterCount << "... ";
-            int newIterCount = gcapp.nextIter();
-            if( newIterCount > iterCount )
-            {
-                gcapp.showImage();
-                cout << iterCount << ">" << endl;
-            }
-            else
-                cout << "rect must be determined>" << endl;
-            break;
-        }
+        gcapp.showImage();
+        cout << iterCount << ">" << endl;
     }
+
+    cvWaitKey(0);
+
 
 exit_main:
     cvDestroyWindow( winName.c_str() );
