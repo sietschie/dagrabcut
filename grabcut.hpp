5028730f37d880e42ed996e233b250540094645e
goering <goering@minet.uni-jena.de>
2011-02-20 17:21:53 +0100

    initial commit: copied grabcut from opencv, compile independently

---
+++ grabcut.hpp
@@ -0,0 +1,13 @@
+#ifndef _GRABCUT_H_
+#define _GRABCUT_H_
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+
+void cg_grabCut( const Mat& img, Mat& mask, Rect rect, 
+                         Mat& bgdModel, Mat& fgdModel,
+                         int iterCount, int mode = GC_EVAL );
+
+
+#endif /* _GRABCUT_H_ */
