32a93dc4a85c7638f9b8c3bba31bff9cde26dd29
goering <goering@minet.uni-jena.de>
2011-02-22 15:46:39 +0100

    put gmm in the header file

--- grabcut.cpp
+++ grabcut.cpp
@@ -54,38 +54,12 @@ This is implementation of image segmentation algorithm GrabCut described in
 Carsten Rother, Vladimir Kolmogorov, Andrew Blake.
  */
 
+#include "grabcut.hpp"
+
 /*
  GMM - Gaussian Mixture Model
 */
-class GMM
-{
-public:
-    static const int componentsCount = 5;
 
-    GMM( Mat& _model );
-    double operator()( const Vec3d color ) const;
-    double operator()( int ci, const Vec3d color ) const;
-    int whichComponent( const Vec3d color ) const;
-
-    void initLearning();
-    void addSample( int ci, const Vec3d color );
-    void endLearning();
-
-private:
-    void calcInverseCovAndDeterm( int ci );
-    Mat model;
-    double* coefs;
-    double* mean;
-    double* cov;
-
-    double inverseCovs[componentsCount][3][3];
-    double covDeterms[componentsCount];
-
-    double sums[componentsCount][3];
-    double prods[componentsCount][3][3];
-    int sampleCounts[componentsCount];
-    int totalSampleCount;
-};
 
 GMM::GMM( Mat& _model )
 {
--- grabcut.hpp
+++ grabcut.hpp
@@ -10,4 +10,34 @@ void cg_grabCut( const Mat& img, Mat& mask, Rect rect,
                          int iterCount, int mode = GC_EVAL );
 
 
+class GMM
+{
+public:
+    static const int componentsCount = 5;
+
+    GMM( Mat& _model );
+    double operator()( const Vec3d color ) const;
+    double operator()( int ci, const Vec3d color ) const;
+    int whichComponent( const Vec3d color ) const;
+
+    void initLearning();
+    void addSample( int ci, const Vec3d color );
+    void endLearning();
+
+private:
+    void calcInverseCovAndDeterm( int ci );
+    Mat model;
+    double* coefs;
+    double* mean;
+    double* cov;
+
+    double inverseCovs[componentsCount][3][3];
+    double covDeterms[componentsCount];
+
+    double sums[componentsCount][3];
+    double prods[componentsCount][3][3];
+    int sampleCounts[componentsCount];
+    int totalSampleCount;
+};
+
 #endif /* _GRABCUT_H_ */
